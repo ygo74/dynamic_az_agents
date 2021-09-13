9035980150a36cb49086ce26a934fd692eef5cd0
ygo74 <yannickgobert@yahoo.fr>
2021-09-13 21:48:12 +0200

    Init pipoeline

---
+++ src/base_agents/windows/start_short.ps1
@@ -0,0 +1,53 @@
+if (-not (Test-Path Env:AZP_URL)) {
+    Write-Error "error: missing AZP_URL environment variable"
+    exit 1
+  }
+
+  if (-not (Test-Path Env:AZP_TOKEN_FILE)) {
+    if (-not (Test-Path Env:AZP_TOKEN)) {
+      Write-Error "error: missing AZP_TOKEN environment variable"
+      exit 1
+    }
+
+    $Env:AZP_TOKEN_FILE = "\azp\.token"
+    $Env:AZP_TOKEN | Out-File -FilePath $Env:AZP_TOKEN_FILE
+  }
+
+  Remove-Item Env:AZP_TOKEN
+
+  if ((Test-Path Env:AZP_WORK) -and -not (Test-Path $Env:AZP_WORK)) {
+    New-Item $Env:AZP_WORK -ItemType directory | Out-Null
+  }
+
+  # Let the agent ignore the token env variables
+  $Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
+
+  # Add capabilities to environment variables
+  $Env:BUILD_UNIQUE_ID = "12345"
+  Set-Location agent
+
+  try
+  {
+    Write-Host "1. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
+
+    .\config.cmd --unattended `
+      --agent "$(if (Test-Path Env:AZP_AGENT_NAME) { ${Env:AZP_AGENT_NAME} } else { ${Env:computername} })" `
+      --url "$(${Env:AZP_URL})" `
+      --auth PAT `
+      --token "$(Get-Content ${Env:AZP_TOKEN_FILE})" `
+      --pool "$(if (Test-Path Env:AZP_POOL) { ${Env:AZP_POOL} } else { 'Default' })" `
+      --work "$(if (Test-Path Env:AZP_WORK) { ${Env:AZP_WORK} } else { '_work' })" `
+      --replace
+
+    Write-Host "2. Running Azure Pipelines agent..." -ForegroundColor Cyan
+
+    .\run.cmd --once
+  }
+  finally
+  {
+    Write-Host "Cleanup. Removing Azure Pipelines agent..." -ForegroundColor Cyan
+
+    .\config.cmd remove --unattended `
+      --auth PAT `
+      --token "$(Get-Content ${Env:AZP_TOKEN_FILE})"
+  }
